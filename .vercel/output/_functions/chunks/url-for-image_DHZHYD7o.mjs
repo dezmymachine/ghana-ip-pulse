ef0c3fdeef013d31094438a3fe3f2e9941704c5f
dezmymachine <desmondkwakucoffie195@gmail.com>
2026-04-01 20:36:28 +0000

    push for vercel

--- .vercel/output/_functions/chunks/url-for-image_DHZHYD7o.mjs
+++
@@ -1,9 +0,0 @@
-import { s as sanityClient } from './page-ssr_CZGsHcuX.mjs';
-import imageUrlBuilder from '@sanity/image-url';
-
-const imageBuilder = imageUrlBuilder(sanityClient);
-function urlForImage(source) {
-  return imageBuilder.image(source);
-}
-
-export { urlForImage as u };
--- .vercel/output/static/_astro/visual-editing-component.DP3nwKwH.js
+++
@@ -1,1 +0,0 @@
-import{V as r}from"./visual-editing-component.uYy-85Ah.js";import"./index.CtgYFdqk.js";import"./index.CcishcJx.js";import"./index.DQnGDcRu.js";export{r as VisualEditingComponent};
